a5de3630d751d1610b5252c9f88373b94797572c
Alina M <189365964+alnm9@users.noreply.github.com>
2025-10-14 13:18:13 +0200

    Add a function that loops through the array and adds book object values to a table

--- script.js
+++ script.js
@@ -16,3 +16,22 @@ function addBookToLibrary(book) {
 const book1 = new Book("The Hobbit", "J.R.R. Tolkien", "295 pages");
 const book2 = new Book("Harry Potter", "J.K. Rowling", "500 pages");
 
+
+
+function addElementToTable() {
+
+    myLibrary.forEach((item) => {
+
+        const objectTr = document.createElement("tr");
+        tableBody.appendChild(objectTr);
+
+        for (let key in item) {
+
+            const keyTh = document.createElement("th");
+            objectTr.appendChild(keyTh);
+            keyTh.textContent = item[key];
+
+        }
+    })
+}
+
